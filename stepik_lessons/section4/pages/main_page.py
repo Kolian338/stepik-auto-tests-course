aeba3b3b03274b4ee6c16c57a5f79552cee7ed13
Nikolay Safonov <kolian338@gmail.com>
2021-07-20 16:41:45 +0700

    added initialization login page to test

--- stepik_lessons/section4/pages/main_page.py
+++ stepik_lessons/section4/pages/main_page.py
@@ -1,5 +1,4 @@
 # Главная страница с возможными действиями
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import MainPageLocators
 
@@ -11,3 +10,4 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
--- stepik_lessons/section4/test_main_page.py
+++ stepik_lessons/section4/test_main_page.py
@@ -1,5 +1,4 @@
 from pages.main_page import MainPage
-from pages.base_page import BasePage
 from pages.login_page import LoginPage
 
 
@@ -8,11 +7,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
-
-def test_registration_and_login_forms_exist(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
