d9549828d23063e0c9ecb9a3570180f18c013c77
Nikolay Safonov <kolian338@gmail.com>
2021-07-28 23:39:28 +0700

    1. Added test for basketPage

---
+++ stepik_lessons/section4/pages/basket_page.py
@@ -0,0 +1,11 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_be_empty_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS)
+
+    def should_be_text_that_basket_is_empty(self):
+        text_basket_empty = self.browser.find_element(*BasketPageLocators.TEXT_THAT_BASKET_IS_EMPTY).text.split(".")[0]
+        assert text_basket_empty == "Your basket is empty"
