904460612e423321e7e4cf6590322861e5d2ea55
Nikolay Safonov <kolian338@gmail.com>
2021-08-01 17:19:53 +0700

    1. fix

--- stepik_lessons/section4/test_main_page.py
+++ stepik_lessons/section4/test_main_page.py
@@ -19,5 +19,5 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
-    basket_page.should_be_text_that_basket_is_empty()  # Переписать, сделать унивирсальную проверку
+    basket_page.should_be_text_about_that_basket_is_empty()  # Переписать, сделать унивирсальную проверку
 
